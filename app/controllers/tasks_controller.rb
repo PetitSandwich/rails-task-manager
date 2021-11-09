d4c5620e599325f4e0e38d26a8b9a31a0d9d4794
antoine pecheux <ant.pecheux@gmail.com>
2021-11-09 14:49:57 +0100

    Exercise 2 Bootstrap and Icons implemented

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,12 @@
 class TasksController < ApplicationController
+
+  before_action :set_tasks, only: [:show, :edit, :update, :destroy]
+
   def index
     @tasks = Task.all
   end
 
   def show
-    @task = Task.find(params[:id])
   end
 
   def new
@@ -17,17 +19,14 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = Task.find(params[:id])
     @task.save
   end
 
   def update
-    @task = Task.find(params[:id])
     @task.update(tasks_params)
   end
 
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
     redirect_to tasks_path
   end
@@ -37,4 +36,9 @@ class TasksController < ApplicationController
   def tasks_params
     params.require(:task).permit(:title, :details, :completed)
   end
+
+  def set_tasks
+    @task = Task.find(params[:id])
+  end
+
 end
